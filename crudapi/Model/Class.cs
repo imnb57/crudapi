69ccf63dc897d2f777e73f2978f5e5bcc0a08aeb
imnb57 <imnitesh56@gmail.com>
2024-11-21 08:09:17 +0545

    Add project files.

---
+++ crudapi/Model/Class.cs
@@ -0,0 +1,12 @@
+﻿public class Student
+{
+    public int StudentID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public string Address { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedBy { get; set; }
+}
